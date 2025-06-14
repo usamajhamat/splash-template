6c7f31c82559fde92e13aef2a42098b93788184f
Muhammad-Usama <107810140+usamajhamat@users.noreply.github.com>
2025-06-14 16:57:56 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+// login.php (your login_url endpoint)
 
+$success_url = $_POST['success_url'] ?? '';
+$username    = $_POST['username'] ?? '';
+$password    = $_POST['password'] ?? '';
+
+// Use the variables as needed
+echo "Success URL: $success_url<br>";
+echo "Username: $username<br>";
+echo "Password: $password<br>";
+?>
